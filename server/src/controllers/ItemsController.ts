bf3c3f19de37e049647b8990ec1d409251b9db59
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-06-05 14:00:19 -0300

    Quarto dia

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
       {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`
+        image_url: `http://192.168.15.11:3333/uploads/${item.image}`
       }
     ))
   
